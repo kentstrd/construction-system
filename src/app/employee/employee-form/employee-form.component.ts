cb876e43cfaeff1c509ca7651c14369870aef304
Alfred Carro <alfredddannvincentfcarro@gmail.com>
2018-08-17 14:18:11 +0800

    CLEAN UP

--- src/app/employee/employee-form/employee-form.component.ts
+++ src/app/employee/employee-form/employee-form.component.ts
@@ -122,7 +122,6 @@ export class EmployeeFormComponent implements OnInit {
     this.addressForms.removeAt(i);
   }
 
- 
   onSubmit() {
     if (this.isNew) {
       this.form.value.id = this.generateId;
@@ -142,5 +141,5 @@ export class EmployeeFormComponent implements OnInit {
         v = c === 'x' ? r : (r & 0x3) | 0x8;
       return v.toString(16);
     });
-  };
+  }
 }
